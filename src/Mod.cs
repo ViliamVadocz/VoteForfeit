468b710b1154bc51470323ec8a5a54da44d7a7de
Viliam Vadocz <viliam.vadocz@gmail.com>
2025-06-11 23:46:37 +0200

    Update to latest Puck version

---
+++ src/Mod.cs
@@ -0,0 +1,62 @@
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace VoteForfeit;
+
+public class Mod : IPuckMod
+{
+    public const string NAME = "VoteForfeit";
+    public const string GUID = $"0x57696c6c.{NAME}";
+    public const string VERSION = "2.0.0";
+
+    private readonly Harmony harmony = new Harmony(GUID);
+
+    public bool OnEnable()
+    {
+        if (!IsDedicatedServer())
+        {
+            LogWarn("This is a server-side mod. It will only work for the client in Practice.");
+        }
+        harmony.PatchAll();
+        LogPatchedMethods();
+        return true;
+    }
+
+    public bool OnDisable()
+    {
+        harmony.UnpatchSelf();
+        return true;
+    }
+
+    public static bool IsDedicatedServer()
+    {
+        return SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null;
+    }
+
+    public void LogPatchedMethods()
+    {
+        LogDebug("Patched these methods:");
+        foreach (var method in harmony.GetPatchedMethods())
+        {
+            if (method.DeclaringType != null) LogDebug($" - {method.DeclaringType.FullName}.{method.Name}");
+        }
+    }
+
+    // Logging wrappers that prefix messages with the GUID.
+
+    public static void LogDebug(object message)
+    {
+        Debug.Log($"[{GUID}] {message}");
+    }
+
+    public static void LogWarn(object message)
+    {
+        Debug.LogWarning($"[{GUID}] {message}");
+    }
+
+    public static void LogError(object message)
+    {
+        Debug.LogError($"[{GUID}] {message}");
+    }
+}
--- src/VoteForfeit.cs
+++ src/VoteForfeit.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using HarmonyLib;
 using Unity.Collections;
 
@@ -9,25 +10,37 @@ namespace VoteForfeit;
 
 public class VoteForfeit
 {
-    private const int minGoalDeficit = 3;
-    private const uint timeoutSeconds = 60;
+    private const int MIN_GOAL_DEFICIT = 3;
+    private const uint TIMEOUT_SECONDS = 60;
 
-    private const string messagePrefix = "<color=orange><b>VoteForfeit</b></color>";
-    private const string helpMessage = $"{messagePrefix} commands:\n"
+    private const string MESSAGE_PREFIX = "<color=orange><b>VoteForfeit</b></color>";
+    private const string HELP_MESSAGE = $"commands:\n"
         + "* <b>/forfeit</b> (/ff) - Vote to forfeit";
-    private const string redTeamText = $"<color=red><b>RED</b></color>";
-    private const string blueTeamText = $"<color=blue><b>BLUE</b></color>";
+    private const string RED_TEAM_TEXT = $"<color=red><b>RED</b></color>";
+    private const string BLUE_TEAM_TEXT = $"<color=blue><b>BLUE</b></color>";
 
+
+    private static void sendMessage(UIChat uiChat, object message)
+    {
+        uiChat.Server_SendSystemChatMessage($"{MESSAGE_PREFIX} {message}");
+    }
+    private static void sendMessage(UIChat uiChat, object message, ulong clientId)
+    {
+        uiChat.Server_SendSystemChatMessage($"{MESSAGE_PREFIX} {message}", clientId);
+    }
+
+    static readonly FieldInfo _uiChat = typeof(UIChatController).GetField("uiChat", BindingFlags.Instance | BindingFlags.NonPublic);
 
     private static Dictionary<FixedString32Bytes, DateTime> votes = [];
 
-    [HarmonyPatch(typeof(UIChatController), nameof(UIChatController.Event_Server_OnChatCommand))]
+    [HarmonyPatch(typeof(UIChatController), "Event_Server_OnChatCommand")]
     public static class UIChatControllerEventServerOnChatCommandPatch
     {
+
         [HarmonyPrefix]
         public static void Event_Server_OnChatCommand(
             UIChatController __instance,
-            Il2CppSystem.Collections.Generic.Dictionary<string, Il2CppSystem.Object> message)
+            Dictionary<string, object> message)
         {
             if (__instance == null) return;
             if (message == null) return;
@@ -36,16 +49,16 @@ public class VoteForfeit
 
             PlayerManager playerManager = NetworkBehaviourSingleton<PlayerManager>.Instance;
             GameManager gameManager = NetworkBehaviourSingleton<GameManager>.Instance;
-            UIChat uiChat = __instance.uiChat;
+            UIChat uiChat = (UIChat)_uiChat.GetValue(__instance);
 
             if (playerManager == null) return;
             if (gameManager == null) return;
             if (uiChat == null) return;
 
             string command = message["command"].ToString().ToLower();
-            ulong clientId = (ulong)Il2CppSystem.Convert.ToInt64(message["clientId"]);
+            ulong clientId = (ulong)message["clientId"];
 
-            if (command.Equals("/help")) { uiChat.Server_SendSystemChatMessage(helpMessage, clientId); return; }
+            if (command.Equals("/help")) { sendMessage(uiChat, HELP_MESSAGE, clientId); return; }
             if (!(command.Equals("/forfeit") || command.Equals("/ff"))) return;
 
             DateTime now = DateTime.Now;
@@ -54,15 +67,15 @@ public class VoteForfeit
             Player player = playerManager.GetPlayerByClientId(clientId);
             FixedString32Bytes steamId = player.SteamId.Value;
             PlayerTeam team = player.Team.Value;
-            Il2CppSystem.Collections.Generic.List<Player> teammates = playerManager.GetPlayersByTeam(team);
+            List<Player> teammates = playerManager.GetPlayersByTeam(team);
             int needed = teammates.Count - 1;
 
             // Check for valid game phase.
             if (gameState.Phase is GamePhase.None or GamePhase.Warmup or GamePhase.GameOver)
             {
-                Plugin.Log.LogDebug($"{clientId} tried to forfeit, but the game phase was {gameState.Phase}.");
-                uiChat.Server_SendSystemChatMessage(
-                    $"{messagePrefix} You cannot vote when there is no match.",
+                Mod.LogDebug($"{clientId} tried to forfeit, but the game phase was {gameState.Phase}.");
+                sendMessage(uiChat,
+                    "You cannot vote to forfeit when there is no match.",
                     clientId
                 );
                 votes.Clear(); // just in case
@@ -76,24 +89,24 @@ public class VoteForfeit
             {
                 case PlayerTeam.Red:
                     goalDeficit = gameState.BlueScore - gameState.RedScore;
-                    teamText = redTeamText;
+                    teamText = RED_TEAM_TEXT;
                     break;
                 case PlayerTeam.Blue:
                     goalDeficit = gameState.RedScore - gameState.BlueScore;
-                    teamText = blueTeamText;
+                    teamText = BLUE_TEAM_TEXT;
                     break;
                 default:
-                    Plugin.Log.LogDebug($"{clientId} tried to forfeit, but they were not on a team.");
-                    uiChat.Server_SendSystemChatMessage($"{messagePrefix} You must be in a team to forfeit.", clientId);
+                    Mod.LogDebug($"{clientId} tried to forfeit, but they were not on a team.");
+                    sendMessage(uiChat, "You must be in a team to forfeit.", clientId);
                     return;
             }
 
             // Check goal deficit.
-            if (goalDeficit < minGoalDeficit)
+            if (goalDeficit < MIN_GOAL_DEFICIT)
             {
-                Plugin.Log.LogDebug($"{clientId} tried to forfeit, but the goal deficit was not large enough: {goalDeficit}<{minGoalDeficit}.");
-                uiChat.Server_SendSystemChatMessage(
-                    $"{messagePrefix} You must be losing by at least {minGoalDeficit} goals to forfeit.",
+                Mod.LogDebug($"{clientId} tried to forfeit, but the goal deficit was not large enough: {goalDeficit}<{MIN_GOAL_DEFICIT}.");
+                sendMessage(uiChat,
+                    $"You must be losing by at least {MIN_GOAL_DEFICIT} goals to forfeit.",
                     clientId
                 );
                 return;
@@ -103,7 +116,7 @@ public class VoteForfeit
             votes = votes
                 .Where(pair =>
                 {
-                    bool recent = now.Subtract(pair.Value).Seconds < timeoutSeconds;
+                    bool recent = now.Subtract(pair.Value).Seconds < TIMEOUT_SECONDS;
                     Player player = playerManager.GetPlayerBySteamId(pair.Key);
                     bool onTheTeam = player != null && player.Team.Value == team;
                     return recent && onTheTeam;
@@ -114,36 +127,36 @@ public class VoteForfeit
 
             if (votes.Count >= needed)
             {
-                Plugin.Log.LogDebug($"Forfeit vote passed.");
-                uiChat.Server_SendSystemChatMessage($"{messagePrefix} {teamText} has forfeited the match.");
+                Mod.LogDebug($"Forfeit vote passed.");
+                sendMessage(uiChat, $"{teamText} has forfeited the match.");
                 votes.Clear();
                 gameManager.Server_GameOver();
             }
             else if (!alreadyVoted)
             {
-                Plugin.Log.LogDebug($"Forfeit vote in progress [{votes.Count}/{needed}].");
-                uiChat.Server_SendSystemChatMessage($"{messagePrefix} Vote <b>forfeit</b> in progress ({votes.Count}/{needed}).");
+                Mod.LogDebug($"Forfeit vote in progress [{votes.Count}/{needed}].");
+                sendMessage(uiChat, $"Vote <b>forfeit</b> in progress ({votes.Count}/{needed}).");
             }
             else
             {
-                Plugin.Log.LogDebug($"{clientId} tried to forfeit, but they already voted recently.");
-                uiChat.Server_SendSystemChatMessage($"{messagePrefix} You already recently voted to forfeit.", clientId);
+                Mod.LogDebug($"{clientId} tried to forfeit, but they already voted recently.");
+                sendMessage(uiChat, "You already recently voted to forfeit.", clientId);
             }
         }
     }
 
-    [HarmonyPatch(typeof(UIChatController), nameof(UIChatController.Event_OnGoalScored))]
+    [HarmonyPatch(typeof(UIChatController), "Event_OnGoalScored")]
     public static class UIChatControllerEventOnGoalScoredPatch
     {
         [HarmonyPrefix]
         public static void Event_OnGoalScored(
             UIChatController __instance,
-            Il2CppSystem.Collections.Generic.Dictionary<string, Il2CppSystem.Object> message)
+            Dictionary<string, object> message)
         {
             if (__instance == null) return;
 
             GameManager gameManager = NetworkBehaviourSingleton<GameManager>.Instance;
-            UIChat uiChat = __instance.uiChat;
+            UIChat uiChat = (UIChat)_uiChat.GetValue(__instance);
 
             if (gameManager == null) return;
             if (uiChat == null) return;
@@ -151,10 +164,10 @@ public class VoteForfeit
             GameState gameState = gameManager.GameState.Value;
             int goalDifference = Math.Abs(gameState.BlueScore - gameState.RedScore);
 
-            if (votes.Count > 0 && goalDifference < minGoalDeficit)
+            if (votes.Count > 0 && goalDifference < MIN_GOAL_DEFICIT)
             {
-                Plugin.Log.LogDebug($"Forfeit vote cancelled because the goal difference fell below the threshold.");
-                uiChat.Server_SendSystemChatMessage($"{messagePrefix} Cancelling <b>forfeit</b> vote.");
+                Mod.LogDebug($"Forfeit vote cancelled because the goal difference fell below the threshold.");
+                sendMessage(uiChat, "Cancelling <b>forfeit</b> vote.");
                 votes.Clear();
             }
         }
